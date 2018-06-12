a5fabbb44d7ff301651a845c365264cddbd987d6
Waseem Ahmed <waseem_ahmed_dxb@outlook.com>
2018-06-12 18:30:09 +0400

    Small refactoring and few comments

--- proto.php
+++ proto.php
@@ -35,17 +35,14 @@ function process(string $filePath) : array
     return $result;
 }
 
-function distanceBasic($lat1, $lon1, $lat2, $lon2) 
+function distanceBasic($lat1, $lon1, $lat2, $lon2)
 {
     // source: https://www.geodatasource.com/developers/php
     $theta = $lon1 - $lon2;
     $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
     $dist = acos($dist);
-    $dist = rad2deg($dist);
-    $miles = $dist * 60 * 1.1515;
 
-    // Miles to KM
-    return ($miles * 1.609344);
+    return radiansToKM($dist);
 }
 
 function distanceVincenty($lat1, $lon1, $lat2, $lon2)
@@ -56,7 +53,16 @@ function distanceVincenty($lat1, $lon1, $lat2, $lon2)
     $divider = sin($lat1) * sin($lat2) + cos($lat1) * cos($lat2) * cos($theta);
 
     $dist = atan2($divider, $divisor);
+
+    return radiansToKM($dist);
+}
+
+function radiansToKM($value)
+{
+    // Radian to (decimal) Degree
     $dist = rad2deg($value);
+
+    // Distance in miles
     $miles = $dist * 60 * 1.1515;
 
     // Miles to KM
