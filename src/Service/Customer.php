b7d7286ead50b0a97ca2e773dfdaf40fae6d5845
Waseem Ahmed <waseem_ahmed_dxb@outlook.com>
2018-06-12 15:14:59 +0400

    Introducing customer service skeleton

---
+++ src/Service/Customer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Waseem\Assessment\Intercom\Service;
+
+/**
+ * Customer service
+ *
+ * @author Waseem Ahmed <waseem_ahmed_dxb@outlook.com>
+ * @version 1.0.0
+ */
+class Customer
+{
+    /**
+     * Invite customers for party
+     * Accepts file to process, scans through it, validate requirements, and finally returns sorted results
+     *
+     * @param string $filePath File local path, compatible for {@see fopen}
+     * @return void
+     */
+    public function Invite(string $filePath)
+    {
+
+        return array();
+    }
+}
