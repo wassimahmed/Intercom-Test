4a0ec10d0d5dac2c4c78348401ace18977d7b477
Waseem Ahmed <waseem_ahmed_dxb@outlook.com>
2018-06-13 11:13:21 +0400

    Introducing unit tests for distance calculators

---
+++ tests/Service/BasicDistanceCalculatorTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Waseem\Assessment\Intercom\Service\DistanceCalculator\Basic as BasicDistanceCalculator;
+
+class BasicDistanceCalculatorTest extends TestCase
+{
+    public function testAllZeros()
+    {
+        $instance = new BasicDistanceCalculator();
+        $result = $instance->Calculate(0.0, 0.0, 0.0, 0.0);
+
+        $this->assertSame(0.0, $result);
+    }
+
+    public function testTwoLatitudesDistance()
+    {
+        $instance = new BasicDistanceCalculator();
+
+        $result = $instance->Calculate(53.0, 0, 54.0, 0);
+
+        // Since each degree of latitude is approx 69 miles (or 111km) apart, our result should be close to it.
+        $this->assertGreaterThan(111.0, $result) and $this->assertLessThan(112.0, $result);
+    }
+}
---
+++ tests/Service/VincentyDistanceCalculatorTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Waseem\Assessment\Intercom\Service\DistanceCalculator\Vincenty as VincentyDistanceCalculator;
+
+class VincentyDistanceCalculatorTest extends TestCase
+{
+    public function testAllZeros()
+    {
+        $instance = new VincentyDistanceCalculator();
+        $result = $instance->Calculate(0.0, 0.0, 0.0, 0.0);
+
+        $this->assertSame(0.0, $result);
+    }
+
+    public function testTwoLatitudesDistance()
+    {
+        $instance = new VincentyDistanceCalculator();
+
+        $result = $instance->Calculate(53.0, 0, 54.0, 0);
+
+        // Since each degree of latitude is approx 69 miles (or 111km) apart, our result should be close to it.
+        $this->assertGreaterThan(111.0, $result) and $this->assertLessThan(112.0, $result);
+    }
+}
