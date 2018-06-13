e4cb6ff1d0ff53ff4c80b61c517e7f48a50bb66d
Waseem Ahmed <waseem_ahmed_dxb@outlook.com>
2018-06-13 11:06:08 +0400

    Bug-fix for Vincenty formula where casting from degress to radians was missing!

--- src/Service/DistanceCalculator/Vincenty.php
+++ src/Service/DistanceCalculator/Vincenty.php
@@ -8,7 +8,7 @@ use Waseem\Assessment\Intercom\Service\DistanceCalculator as base;
  * Vincenty formula based distance calculator
  *
  * @author Waseem Ahmed <waseem_ahmed_dxb@outlook.com>
- * @version 1.0.0
+ * @version 1.1.0
  */
 class Vincenty extends base
 {
@@ -24,12 +24,17 @@ class Vincenty extends base
      */
     public function Calculate(float $lat1, float $lon1, float $lat2, float $lon2) : float
     {
-        $theta = $lon1 - $lon2;
+        $theta = deg2rad($lon1 - $lon2);
+
+        $lat1 = deg2rad($lat1);
+        $lon1 = deg2rad($lon1);
+        $lat2 = deg2rad($lat2);
+        $lon2 = deg2rad($lon2);
 
         $divisor = sqrt(pow(cos($lat2) * sin($theta), 2) + pow(cos($lat1) * sin($lat2) - sin($lat1) * cos($lat2) * cos($theta), 2));
         $divider = sin($lat1) * sin($lat2) + cos($lat1) * cos($lat2) * cos($theta);
-    
-        $dist = atan2($divider, $divisor);
+
+        $dist = atan($divisor / $divider);
 
         return parent::RadiansToKilometers($dist);
     }
