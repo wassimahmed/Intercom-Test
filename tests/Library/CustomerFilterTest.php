2adae6938542a38b9d4524327471a6d379fd6af0
Waseem Ahmed <waseem_ahmed_dxb@outlook.com>
2018-06-13 08:17:25 +0400

    Adding unit test for Customer Filter

---
+++ tests/Library/CustomerFilterTest.php
@@ -0,0 +1,52 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Waseem\Assessment\Intercom\Library\CustomerFilter;
+
+class CustomerFilterTest extends TestCase
+{
+    public function testValidJson()
+    {
+        $iterator = new \ArrayIterator([
+            '{"user_id": "10", "name": "someone", "latitude": "53.1234", "longitude": "23.1234"}',
+        ]);
+
+        $instance = new CustomerFilter($iterator);
+        $instance->rewind();
+
+        $this->assertTrue($instance->valid());
+
+        $record = $instance->current();
+        $this->assertTrue(is_array($record));
+
+        $this->assertArrayHasKey('user_id', $record);
+        $this->assertSame($record['user_id'], 10);
+
+        $this->assertArrayHasKey('name', $record);
+        $this->assertSame($record['name'], 'someone');
+
+        $this->assertArrayHasKey('latitude', $record);
+        $this->assertSame($record['latitude'], floatval('53.1234'));
+
+        $this->assertArrayHasKey('longitude', $record);
+        $this->assertSame($record['longitude'], floatval('23.1234'));
+    }
+
+    public function testInvalidJson()
+    {
+        $iterator = new \ArrayIterator([
+            'This is not a valid JSON record, hence must be filtered out',
+            '{"invalid": "true", "reason": "Invalid customer record structure"}',
+            '{"name": "oops-missing-id", "latitude": "53.1234", "longitude": "23.1234"}',
+            '[{"user_id": "10", "name": "this-is-invalid-too;nested!", "latitude": "53.1234", "longitude": "23.1234"}]',
+        ]);
+
+        $instance = new CustomerFilter($iterator);
+        $instance->rewind();
+
+        $this->assertFalse($instance->valid());
+
+        $record = $instance->current();
+        $this->assertNull($record);
+    }
+}
